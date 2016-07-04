d6e324601c55470ea0c5bab6b15908a132f93180
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-07-04 23:40:41 +0200

    Reuse IP packet in tcp_connect.

--- tcp_connect.py
+++ tcp_connect.py
@@ -4,16 +4,17 @@ import os
 from addr import *
 from scapy.all import *
 
-pid=os.getpid() & 0xffff
+ip=IP(src=FAKE_NET_ADDR, dst=REMOTE_ADDR)
+port=os.getpid() & 0xffff
 
 print "Send SYN packet, receive SYN+ACK."
-syn=TCP(sport=pid, dport='chargen', seq=1, flags='S', window=(2**16)-1)
-synack=sr1(IP(src=FAKE_NET_ADDR, dst=REMOTE_ADDR)/syn, iface=LOCAL_IF)
+syn=TCP(sport=port, dport='chargen', seq=1, flags='S', window=(2**16)-1)
+synack=sr1(ip/syn, iface=LOCAL_IF)
 
 print "Send ack packet, receive chargen data."
 ack=TCP(sport=synack.dport, dport=synack.sport, seq=2, flags='A',
     ack=synack.seq+1, window=(2**16)-1)
-data=sr1(IP(src=FAKE_NET_ADDR, dst=REMOTE_ADDR)/ack, iface=LOCAL_IF)
+data=sr1(ip/ack, iface=LOCAL_IF)
 
 print "Fill our receive buffer."
 time.sleep(1)
@@ -24,12 +25,12 @@ icmp=ICMP(type="dest-unreach", code="fragmentation-needed",
 send(IP(src=LOCAL_ADDR, dst=REMOTE_ADDR)/icmp, iface=LOCAL_IF)
 
 print "Path MTU discovery will resend first data with length 1300."
-data=sr1(IP(src=FAKE_NET_ADDR, dst=REMOTE_ADDR)/ack, iface=LOCAL_IF)
+data=sr1(ip/ack, iface=LOCAL_IF)
 
 print "Cleanup the other's socket with a reset packet."
 rst=TCP(sport=synack.dport, dport=synack.sport, seq=2, flags='AR',
     ack=synack.seq+1)
-send(IP(src=FAKE_NET_ADDR, dst=REMOTE_ADDR)/rst, iface=LOCAL_IF)
+send(ip/rst, iface=LOCAL_IF)
 
 len = data.len
 print "len=%d" % len
