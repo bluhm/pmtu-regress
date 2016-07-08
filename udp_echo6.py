565c244c59b5252c157d8e1453280b2aca2b18e5
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-07-08 18:45:07 +0200

    Get UDP fragment answer with fork() an sniff(), srp1() is unusable.

--- udp_echo6.py
+++ udp_echo6.py
@@ -16,9 +16,6 @@ data=''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase +
 udp=UDP(sport=port, dport='echo')/data
 echo=srp1(e/ip6/udp, iface=LOCAL_IF, timeout=5)
 
-print "Fill our fragment cache."
-time.sleep(1)
-
 print "Send ICMP6 packet too big packet with MTU 1300."
 icmp6=ICMPv6PacketTooBig(mtu=1300)/echo.payload
 sendp(e/IPv6(src=LOCAL_ADDR6, dst=REMOTE_ADDR6)/icmp6, iface=LOCAL_IF)
@@ -26,21 +23,33 @@ sendp(e/IPv6(src=LOCAL_ADDR6, dst=REMOTE_ADDR6)/icmp6, iface=LOCAL_IF)
 print "Clear route cache at echo socket by sending to different address."
 sendp(e/IPv6(src="fdd7:e83e:66bc:188::", dst=REMOTE_ADDR6)/udp, iface=LOCAL_IF)
 
-class UDPfrag6(UDP):
-    def hashret(self):
-	return "X"
-    def answers(self, other):
-	return 1
-
 print "Path MTU discovery will send UDP fragment with length 1300."
-udpfrag6=IPv6ExtHdrFragment()/UDP(sport=port, dport='echo')/data
-p=e/ip6/udpfrag6
-p.show2()
-frag=srp1(e/ip6/udpfrag6, nofilter=1, iface=LOCAL_IF, timeout=5)
+# srp1 cannot be used, fragment answer will not match on outgoing udp packet
+if os.fork() == 0:
+        time.sleep(1)
+        sendp(e/ip6/udp, iface=LOCAL_IF)
+        os._exit(0)
+
+ans=sniff(iface=LOCAL_IF, timeout=3, filter=
+    "ip6 and src "+ip6.dst+" and dst "+ip6.src+" and proto ipv6-frag")
+
+for a in ans:
+	fh=a.payload.payload
+	if fh.offset != 0 or fh.nh != (ip6/udp).nh:
+		continue
+	uh=fh.payload
+	if uh.sport != udp.dport or uh.dport != udp.sport:
+		continue
+	frag=a
+	break
+else:
+	print "ERROR: no matching IPv6 fragment UDP answer found"
+	exit(1)
 
 len = frag.plen + len(IPv6())
 print "len=%d" % len
-if len != 1300:
-	print "ERROR: UDP fragment len is %d, expected 1300." % len
+# fragments contain multiple of 8 octets, so expected len is 1296
+if len != 1296:
+	print "ERROR: UDP fragment len is %d, expected 1296." % len
 	exit(1)
 exit(0)
